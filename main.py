278cacc9f55de2fe1b947382cf0d1ca6d2ada8b1
yyoongs <yyoongss@gmail.com>
2025-09-21 02:05:42 +0900

    워커 수 변경

--- main.py
+++ main.py
@@ -609,7 +609,7 @@ if __name__ == "__main__":
             "main:app",
             host="0.0.0.0",
             port=8000,
-            workers=1,
+            workers=4,
             reload=True,
             log_level="info"
         )
